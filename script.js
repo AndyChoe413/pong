9de96f0e6814bdc1b21327a4ce0bae4e5cc8dce4
Andy Choe <Andrew.s.choe@gmail.com>
2021-03-17 12:01:31 -0400

    assignment due

--- script.js
+++ script.js
@@ -14,19 +14,60 @@ const computerPaddle = document.querySelector('.computer-paddle');
 
 // Initial computer paddle y-position and y-velocity
 let computerPaddleYPosition = 0;
-let computerPaddleYVelocity = 1;
+let computerPaddleYVelocity = 3;
+
+// Get the ball element
+const ball = document.querySelector('.ball')
+
+//initial ball position
+let ballXPosition = 0;
+let ballYPosition = 0;
+let ballXVelocity = 3;
+let ballYVelocity = 1;
 
 // Update the pong world
 function update() {
 
+    //-----------------ball code----------------------------//
+
+    // Update ball position
+    ballXPosition += ballXVelocity
+    ballYPosition += ballYPosition
+
+    //If ball goes off screen bring it back
+    ballXPosition = ballXPosition % (GAME_AREA_HEIGHT - BALL_SIZE);
+    ballYPosition = ballYPosition % (GAME_AREA_WIDTH - BALL_SIZE);
+
+    //Apply the balls y-position
+    ball.style.left = `${ballXPosition}px`;
+
+    //Apply the balls x-position
+    ball.style.top = `${ballYPosition}px`;
+    
+
+    // ---------------Paddle code----------------------------//
+
+    
     // Update the computer paddle's position
     computerPaddleYPosition = computerPaddleYPosition + computerPaddleYVelocity;
+
+    if (computerPaddleYPosition + PADDLE_HEIGHT >= GAME_AREA_HEIGHT) {
+        computerPaddleYPosition -= 2;
+        computerPaddleYPosition = GAME_AREA_HEIGHT - PADDLE_HEIGHT;
+    }
+
+     if (computerPaddleYPosition - PADDLE_HEIGHT <= -1) {
+        computerPaddleYPosition = PADDLE_HEIGHT;
+    }
+
 
     // If the computer paddle goes off the edge of the screen, bring it back
     computerPaddleYPosition = computerPaddleYPosition % (GAME_AREA_HEIGHT - PADDLE_HEIGHT);
 
     // Apply the y-position 
     computerPaddle.style.top = `${computerPaddleYPosition}px`;
+
+    console.log(computerPaddleYPosition)
 }
 
 // Call the update() function everytime the browser is ready to re-render
